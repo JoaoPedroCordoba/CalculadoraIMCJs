820f3d39d3202ec407fbb2dcdf67ad4a070fae58
joaoPedro <e107ti@cps.sp.gov.br>
2022-11-22 18:35:18 -0300

    finalizar o css

--- index.js
+++ index.js
@@ -1,10 +1,76 @@
-let massa = document.getElementById("massa");
-let altura = document.getElementById("altura");
-let resultado = document.getElementById("resultado");
+// Adiciona variáveis para armazenar campos necessários
+let campoAltura = null;
+let campoMassa = null;
+let botaoQueCalcula = null;
+let paragrafoExibeIMC = null;
+let paragrafoExibeSituacao = null;
+let divExibeResultado = null;
 
-let imc = massa / (altura - altura) 
+// Armazena altura e massa
+let dadosUsuario = {
+    altura: null,
+    massa: null
+}
 
-if(imc<17)
-{
-    resultado = "Muito abaixo do peso";
+// Chama Setup quando o DOM for carregado
+document.addEventListener("DOMContentLoaded", setup);
+
+function setup() {
+    // Obtém os campos necessários
+    campoAltura = document.getElementById("alturaUsuario");
+    campoMassa = document.getElementById("massaUsuario");
+    botaoQueCalcula = document.getElementById("botaoCalcular");
+    paragrafoExibeIMC = document.getElementById("exibeIMC");
+    paragrafoExibeSituacao = document.getElementById("exibeSituacao");
+    divExibeResultado = document.getElementById("exibeCalculos");
+
+    // Quando o campoAltura muda, o valor nele é armazenado na propriedade "altura" do objeto dadosUsuário
+    campoAltura.addEventListener("change", () => dadosUsuario.altura = Number(campoAltura.value));
+
+    // Quando o campoMassa muda, o valor nele é armazenado na propriedade "massa" do objeto dadosUsuário
+    campoMassa.addEventListener("change", () => dadosUsuario.massa = Number(campoMassa.value));
+
+    // Quando o usuário clica no botão que calcula, o IMC é calculado, a situação é averiguada e os resultados são exibidos
+    botaoQueCalcula.addEventListener("click", () => {
+        if (dadosUsuario.altura != 0 && dadosUsuario.massa != 0) {
+        const valorIMC = (dadosUsuario.massa / (dadosUsuario.altura ** 2)).toFixed(2);
+        const situacaoUsuario = testaSituacao(valorIMC);
+
+        paragrafoExibeIMC.innerText = `${valorIMC} kg/m².`;
+        paragrafoExibeSituacao.innerText = `${situacaoUsuario}`;
+        divExibeResultado.hidden = 0;
+        }
+    });
+}
+
+function testaSituacao(valorIMC) {
+    // Assegura que o valor de IMC usado será numérico
+    let IMC = Number(valorIMC)
+
+    // Averigua a situação do usuário
+    switch (true) {
+        case (IMC < 17):
+            return "Muito abaixo do peso.";
+
+        case (IMC < 18.5):
+            return "Abaixo do peso."
+        
+        case (IMC < 25):
+            return "Peso normal.";
+
+        case (IMC < 30):
+            return "Acima do peso.";
+
+        case (IMC < 35):
+            return "Obesidade.";
+
+        case (IMC < 40):
+            return "Obesidade severa.";
+
+        case (IMC >= 40):
+            return "Obesidade mórbida.";
+
+        default:
+            return "Um erro ocorreu.";
+    }
 }
